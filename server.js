84fcac5d94087ce3e3313ee8dafd5cea38d94a49
bosco0202 <abduvosid1177@gmail.com>
2025-06-02 23:21:14 +0900

    feat:devalop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 /// 1: Kirish code
 app.use(express.static("public"));
@@ -16,11 +26,17 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 ///4: Routing code
-app.get("/hello", function (req, res) {
-  res.end("<h1>HELLO BOSCO</h1>");
+app.post("/create-item", (req, res) => {
+  // console.log(req.body);
+  // res.json({ test: "success" });
 });
-app.get("/gift", function (req, res) {
-  res.end("<h1>HELLO BOSCO</h1>");
+
+app.get(`/author`, (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get(`/`, function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
--- train.js
+++ train.js
@@ -1,1 +1,1 @@
-console.log("Teain area");
+
