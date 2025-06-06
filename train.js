8827cc681e3c002e6568ff8909f9faf7bb07472a
bosco0202 <abduvosid1177@gmail.com>
2025-06-06 19:40:22 +0900

    feat:train callback hamda asyn function

--- train.js
+++ train.js
@@ -7,26 +7,43 @@ const list = [
   "yoshlarga investitsiya qiling", //50-60
   "endi dam oling, foydasi toq endi", //60
 ];
-
-function maslahatBering(a, callback) {
-  if (typeof a !== `number`) callback("insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
+//Async function
+async function maslahatBering(a) {
+  if (typeof a !== "number") throw new Error("insert a number", null);
+  else if (a <= 20) return list[0];
+  else if (a > 20 && a <= 30) return list[1];
+  else if (a > 30 && a <= 40) return list[2];
+  else if (a > 40 && a <= 50) return list[3];
+  else if (a > 50 && a <= 60) return list[4];
   else {
-    setTimeout(function () {
-      callback(null, list[5]);
-    }, 5000);
+    ////Promise function qayta koramz
+    return new Promise((resolve, rejact) => {
+      setTimeout(function () {
+        resolve(null, list[5]);
+      }, 5000);
+    });
   }
 }
+///call via then/catch
+// // console.log("passed here 0");
+// // maslahatBering(20)
+// //   .then((data) => {
+// //     console.log("javob:", data);
+// //   })
+// //   .catch((err) => {
+// //     console.log("ERROR:", err);
+// //   });
+// // console.log("passed here 1");
 
-console.log("passed here 0");
-maslahatBering(65, (err, data) => {
-  if (err) console.log(`ERROR:`, err);
-  else {
-    console.log(`javob:`, data);
-  }
-});
-console.log("passed here 1");
+// //asyn await
+// async function run() {
+//   let javob = await maslahatBering(65);
+//   console.log(javob);
+//   javob = await maslahatBering(31);
+//   console.log(javob);
+//   javob = await maslahatBering(40);
+//   console.log(javob);
+//   javob = await maslahatBering(50);
+//   console.log(javob);
+// }
+// run();
