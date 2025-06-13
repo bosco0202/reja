61dbaabb25b99cd9017dc2dbce93fb2a01f994f7
bosco0202 <abduvosid1177@gmail.com>
2025-06-13 14:47:03 +0900

    feat:Task C

--- train.js
+++ train.js
@@ -91,3 +91,73 @@
 //   return count;
 // }
 // console.log(countDigits("ad2a54y79wet0sfgb9"));
+
+// TASK-C
+
+// Shop nomli class tuzing, va bu class 3 xill parametr qabul qilsin.
+// Hamda classning quyidagdek 3'ta metodi bo'lsin:
+
+// 1) qoldiq
+// 2) sotish
+// 3) qabul
+
+// Har bir metod ishga tushgan vaqtda log qilinsin
+
+// MASALAN:
+// const shop = new Shop(4, 5, 2)
+
+// shop.qoldiq();
+// natija qaytishi kerak: Hozir 20: 40'da 4'ta non, 5'ta lag'mon va 2'ta cola mavjud
+
+// shop.sotish("non", 3); & shop.qabul("cola", 4); & shop.qoldiq();
+// Natija qaytishi kerak: Hozir 20:50da 1ta non, 5ta lag'mon va 6ta cola mavjud!
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+  _getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
+  }
+
+  qoldiq() {
+    const time = this._getCurrentTime();
+    console.log(
+      `Hozir ${time}da ${this.non}ta non, ${this.lagmon}ta lag'mon va ${this.cola}ta cola mavjud`
+    );
+  }
+
+  sotish(tovar, miqdor) {
+    if (this[tovar] === undefined) {
+      console.log(`Do'konda ${tovar} yo'q`);
+      return;
+    }
+    if (this[tovar] < miqdor) {
+      console.log(`Do'konda ${tovar} yetarli emas`);
+      return;
+    }
+    this[tovar] -= miqdor;
+    const time = this._getCurrentTime();
+    console.log(`Sotildi: ${miqdor}ta ${tovar} (${time})`);
+  }
+
+  qabul(tovar, miqdor) {
+    if (this[tovar] === undefined) {
+      console.log(`Do'konga yangi tovar qo'shildi: ${tovar}`);
+      this[tovar] = 0;
+    }
+    this[tovar] += miqdor;
+    const time = this._getCurrentTime();
+    console.log(`Qabul qilindi: ${miqdor}ta ${tovar} (${time})`);
+  }
+}
+const shop = new Shop(4, 5, 2);
+shop.qoldiq();
+shop.sotish("non", 3);
+shop.qabul("cola", 4);
+shop.qoldiq();
